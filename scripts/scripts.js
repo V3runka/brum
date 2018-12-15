e694b141b1cbcca913256f647bb053eb396abdc8
Filip Jůza <filip@unchained.studio>
2018-12-15 15:02:47 +0100

    Remove slider

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,35 +9,3 @@ menuButton.addEventListener(
     },
     false
 );
-
-if (document.querySelector('.slideshow-container')) {
-    let slideIndex = 1;
-
-    showSlide(slideIndex);
-
-    function plusSlides(n) {
-        showSlide((slideIndex += n));
-    }
-
-    function currentSlide(n) {
-        showSlide((slideIndex = n));
-    }
-
-    function showSlide(n) {
-        var slides = document.getElementsByClassName('mySlides');
-
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-        }
-
-        slides[slideIndex - 1].style.display = 'block';
-    }
-}
